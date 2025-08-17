bf0c03a3d3b9d0b2f2ff6850177f625cd000aa59
JoPim <joanantonio95@gmail.com>
2025-08-16 20:37:50 -0400

    Delete src directory

--- src/blackjack/usecases/accumulate-points.js
+++
@@ -1,7 +0,0 @@
-import { cardValue } from './card-value';
-
-export const accumulatePoints = (card, turn, playersPoints, scrAccum) => {
-  playersPoints[turn] += cardValue(card);
-  scrAccum[turn].innerText = playersPoints[turn];
-  return playersPoints[turn];
-};
